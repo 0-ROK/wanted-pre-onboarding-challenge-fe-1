e0fc7181ca19d10b7253543836ec6b0adb8b5b03
조영록 <54888076+0-ROK@users.noreply.github.com>
2022-12-15 23:37:33 +0900

    Todo 페이지 일부 구현
    - todo 추가하기 기능을 구현했습니다.
    - todo 체크/체크 해제 기능을 구현했습니다.

---
+++ front/src/hooks/useTodo.ts
@@ -0,0 +1,71 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export interface CreateTodoPayload {
+  todo: string;
+}
+
+export interface ToDoItem {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
+
+export const useTodo = () => {
+  const createTodo = async (payload: CreateTodoPayload) => {
+    try {
+      const response = await axios.post("/todos", payload, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.status !== 201) throw Error("실패");
+      getTodos();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const [todos, setTodos] = useState<Array<ToDoItem>>([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await axios.get("/todos", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+      if (response.status !== 200) throw Error("실패");
+      setTodos(response.data);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  interface UpdateTodoPayload {
+    todo: string;
+    isCompleted: boolean;
+  }
+
+  const updateTodo = async (id: number, payload: UpdateTodoPayload) => {
+    try {
+      const response = await axios.put(`/todos/${id}`, payload, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+      if (response.status !== 200) throw Error("실패");
+      getTodos();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return { createTodo, getTodos, todos, updateTodo };
+};
